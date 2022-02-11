44c2d1ee98d471b73a97d67afa0d51d4c82b6e29
Rhutik Giradkar <46106467+rhutikcodes@users.noreply.github.com>
2022-02-11 18:20:01 +0530

    TASK: injectable config

---
+++ lib/injection.config.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+import 'package:firebase_auth/firebase_auth.dart' as _i5;
+import 'package:get_it/get_it.dart' as _i1;
+import 'package:injectable/injectable.dart' as _i2;
+
+import 'data/auth/repository/firebase_auth_facade.dart' as _i4;
+import 'domain/auth/repository/i_auth_facade.dart'
+    as _i3; // ignore_for_file: unnecessary_lambdas
+
+// ignore_for_file: lines_longer_than_80_chars
+/// initializes the registration of provided dependencies inside of [GetIt]
+_i1.GetIt $initGetIt(_i1.GetIt get,
+    {String? environment, _i2.EnvironmentFilter? environmentFilter}) {
+  final gh = _i2.GetItHelper(get, environment, environmentFilter);
+  gh.lazySingleton<_i3.IAuthFacade>(
+      () => _i4.FirebaseAuthFacade(get<_i5.FirebaseAuth>()));
+  return get;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:injectable/injectable.dart';
 
-void main() => runApp(MyApp());
+import 'injection.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  configureInjection(Environment.prod);
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Material App Bar'),
+          title: const Text('Material App Bar'),
         ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
+        body: const Center(
+          child: Text('Hello World'),
         ),
       ),
     );
