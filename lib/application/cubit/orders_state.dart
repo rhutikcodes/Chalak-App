564d7713c78504159c2717cb8d78b8145fd1bbe7
Rhutik Giradkar <46106467+rhutikcodes@users.noreply.github.com>
2022-02-14 14:00:37 +0530

    TASK: get bookings cubit

--- lib/application/cubit/orders_state.dart
+++ lib/application/cubit/orders_state.dart
@@ -3,4 +3,5 @@ part of 'orders_cubit.dart';
 @freezed
 class OrdersState with _$OrdersState {
   const factory OrdersState.initial() = _Initial;
+  const factory OrdersState.loaded(List<OrderEntity> orderEnityList) = _Loaded;
 }
